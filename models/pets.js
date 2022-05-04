ad5f8a92c354c93b952ffb393e59fc49f7e0f99f
Elvis Fagundes <elvis.f.lisboa.jr@hotmail.com>
2022-05-04 19:59:40 -0300

    Curso de NodeJS: Streaming de dados e Repositório M2

--- models/pets.js
+++ models/pets.js
@@ -1,17 +1,28 @@
 const conexao = require('../infraestrutura/conexao')
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
 
 class Pet {
     adiciona(pet, res) {
         const query = 'INSERT INTO Pets ?'
 
-        conexao.query(query, pet, erro => {
-            if(erro) {
-                console.log(erro)
-                res.status(400).json(erro)
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+
+            if (erro) {
+                res.status(400).json({ erro })
             } else {
-                res.status(200).json(pet)
+                const novoPet = { nome: pet.nome, imagem: novoCaminho }
+                conexao.query(query, pet, erro => {
+                    if (erro) {
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(200).json(pet)
+                    }
+                })
             }
+
         })
     }
 }
+
 module.exports = new Pet()
